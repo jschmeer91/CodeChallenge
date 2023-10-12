882259f48576ef19279ec31165a3039ce002b5dd
Jerry Schmeer <jerry.schmeer@tmasystems.com>
2023-10-12 13:08:18 -0700

    Added Compensation Type, and 2 endpoints to create/update compensation and to read them out to the API

---
+++ CodeChallenge/Controllers/CompensationController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CodeChallenge.Services;
+using CodeChallenge.Models;
+
+namespace CodeChallenge.Controllers
+{
+    [ApiController]
+    [Route("api/compensation")]
+    public class CompensationController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly ICompensationService _compensationService;
+
+        public CompensationController(ILogger<CompensationController> logger, ICompensationService compensationService)
+        {
+            _logger = logger;
+            _compensationService = compensationService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCompensation([FromBody] Compensation compensation)
+        {
+            _logger.LogDebug($"Received compensation create request for employeeId '{compensation.EmployeeId}'");
+
+            compensation = _compensationService.Create(compensation);
+            if (compensation == null)
+                return NotFound("Employee does not exist.");
+
+            return CreatedAtRoute("getCompensationById", new { id = compensation.EmployeeId }, compensation);
+        }
+
+        [HttpGet("{id}", Name = "getCompensationById")]
+        public IActionResult Compensation(String id)
+        {
+            _logger.LogDebug($"Received employee get request for '{id}'");
+
+            var compensation = _compensationService.GetByEmployeeId(id);
+
+            if (compensation == null)
+                return NotFound();
+
+            return Ok(compensation);
+        }
+    }
+}
---
+++ CodeChallenge/Data/CompensationContext.cs
@@ -0,0 +1,20 @@
+﻿using CodeChallenge.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeChallenge.Data
+{
+    public class CompensationContext : DbContext
+    {
+        public CompensationContext(DbContextOptions<CompensationContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Compensation>()
+                .HasKey(e => e.EmployeeId);
+        }
+
+        public DbSet<Compensation> Compensations { get; set; }
+    }
+}
